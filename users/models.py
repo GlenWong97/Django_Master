b0f90875849b7a70f67b780ae1b464ab05f36a8a
tzw0 <e0310410@u.nus.edu>
2019-06-16 19:34:17 +0800

    profile pics

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default = 'profile_pics/default.jpg', upload_to='profile_pics')
+    gender = models.CharField(
+        max_length = 12,
+        choices = [
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Others', 'Others'),
+            ('Justin Lu', 'Justin Lu')
+        ],
+        default = 'Justin Lu'
+    )
+    bio = models.CharField(max_length = 1000, blank = True, default = '-')
+    def __str__(self):
+        return f'{self.user.username} Profile'
