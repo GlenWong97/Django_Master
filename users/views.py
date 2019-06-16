185873df47030ca1bb08584119af64c150a9a29b
tzw0 <e0310410@u.nus.edu>
2019-06-16 16:55:53 +0800

    some general colours

--- users/views.py
+++ users/views.py
@@ -15,6 +15,10 @@ def register(request):
         form =  UserRegisterForm
     return render(request, 'users/register.html', {'form' : form})
 
+def login(request):
+    if login.method == 'POST':
+        messages.success(request, f'Welcome {username}, great to have you back!')
+
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
