d31ed0e0abdf14e01ebceffef1937a6a7e11ea2e
tzw0 <e0310410@u.nus.edu>
2019-06-21 15:16:55 +0800

    .

--- store/static/store/main.js
+++ store/static/store/main.js
@@ -8,15 +8,4 @@ function myFunction() {
   }
 }
 
-$(function () {
-    $('#homeCarousel').carousel({
-        interval:2000,
-        pause: "false"
-    });
-    $('#playButton').click(function () {
-        $('#homeCarousel').carousel('cycle');
-    });
-    $('#pauseButton').click(function () {
-        $('#homeCarousel').carousel('pause');
-    });
-});
+
