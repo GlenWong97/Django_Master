d9108e81a5beafb850110814825ac3a545085ab7
tzw0 <e0310410@u.nus.edu>
2019-06-21 21:40:30 +0800

    left saving image

--- store/models.py
+++ store/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from PIL import Image
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
+	image = models.ImageField(default = 'default0.jpg', upload_to='course_image')
 	description = models.TextField()
 	price = models.DecimalField(decimal_places=2, max_digits=6)
 	date_posted = models.DateTimeField(default=timezone.now)
@@ -16,4 +18,11 @@ class Post(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk' : self.pk})
-	
+
+	def save(self, *args, **kwargs):
+         super(Post, self).save(*args, **kwargs)
+         img = Image.open(self.image.path)
+         if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
